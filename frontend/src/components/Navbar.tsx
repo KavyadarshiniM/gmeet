6116dcada4eeee718adba2ae2bb1275eec964823
KavyadarshiniM <kavyadarshinim2005@gmail.com>
2026-04-29 12:23:47 +0530

    Commit 2

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,9 +10,9 @@ export const Navbar: React.FC = () => {
             <Link href="/" className="nav-logo">MEET<span>.</span>PULSE</Link>
             <ul className="nav-links">
                 <li><Link href="/#features">Features</Link></li>
-                <li><Link href="/#how">Proceeeess</Link></li>
+                <li><Link href="/#how">Process</Link></li>
                 <li><Link href="/analytics">Analytics</Link></li>
-                <li><Link href="/calendar">Calenderr</Link></li>
+                <li><Link href="/calendar">Calender</Link></li>
                 <li><Link href="/generator">Generators</Link></li>
             </ul>
             <Link href="/generator" className="nav-cta"><span>Generate Now ↓</span></Link>
